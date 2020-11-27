8f685e50df31cc8be7f5b4f90d626b39cc6d6666
Czandi <kasprzakdominik@outlook.com>
2020-11-27 15:01:02 +0100

    Add insert subject endpoint

--- backend/src/main/java/com/app/iami/model/Subject.java
+++ backend/src/main/java/com/app/iami/model/Subject.java
@@ -1,7 +1,10 @@
 package com.app.iami.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +13,9 @@ import javax.persistence.Id;
 
 @Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Subject {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
