1459f6dbda7fdf9c4eba9fba538654ffe262e39a
Czandi <kasprzakdominik@outlook.com>
2020-12-01 16:18:43 +0100

    Working on add course component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,17 +5,19 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './components/nav/nav.component';
 import { LoginComponent } from './components/login/login.component';
-import { MaterialModule} from "./material";
-import { appRoutingModule } from "./app.routing";
-import { TeacherHubComponent } from "./components/teacher-hub/teacher-hub.component";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { HttpClientModule} from "@angular/common/http";
+import { MaterialModule } from './material';
+import { appRoutingModule } from './app.routing';
+import { TeacherHubComponent } from './components/teacher-hub/teacher-hub.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AddCourseComponent } from './components/teacher-hub/add-course/add-course.component';
 import { ListCourseComponent } from './components/teacher-hub/list-course/list-course.component';
 import { AddSubjectComponent } from './components/teacher-hub/add-subject/add-subject.component';
 import { SingleCourseComponent } from './components/teacher-hub/list-course/single-course/single-course.component';
-import {authInterceptorProviders} from "./helpers/auth.guard";
-
+import { authInterceptorProviders } from './helpers/auth.guard';
+import { MatSelectModule } from '@angular/material/select';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -26,11 +28,9 @@ import {authInterceptorProviders} from "./helpers/auth.guard";
     AddCourseComponent,
     ListCourseComponent,
     AddSubjectComponent,
-    SingleCourseComponent
+    SingleCourseComponent,
   ],
-  exports: [
-    MaterialModule
-  ],
+  exports: [MaterialModule],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -38,12 +38,12 @@ import {authInterceptorProviders} from "./helpers/auth.guard";
     appRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    NgxMatSelectSearchModule,
   ],
-  providers: [
-    authInterceptorProviders
-
-  ],
-  bootstrap: [AppComponent]
+  providers: [authInterceptorProviders],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
