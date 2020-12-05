4b4574b92159760414e40285a48c429ed8fd9ea1
Czandi <kasprzakdominik@outlook.com>
2020-12-05 17:48:15 +0100

    Add presence delete functionality

--- frontend/src/app/components/teacher-hub/list-course/single-course/single-course.component.ts
+++ frontend/src/app/components/teacher-hub/list-course/single-course/single-course.component.ts
@@ -1,71 +1,65 @@
-import { Component, OnDestroy, OnInit} from '@angular/core';
-import {ActivatedRoute, Route, Router} from "@angular/router";
-import {Subscription} from "rxjs";
-import {CourseService} from "../../../../services/course.service";
-import {FormControl} from "@angular/forms";
-import {Presence} from "../../../../models/presence.model";
-
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { CourseService } from '../../../../services/course.service';
+import { FormControl } from '@angular/forms';
+import { Presence } from '../../../../models/presence.model';
 
 @Component({
   selector: 'app-single-course',
   templateUrl: './single-course.component.html',
-  styleUrls: ['./single-course.component.css']
+  styleUrls: ['./single-course.component.css'],
 })
-export class SingleCourseComponent implements OnInit, OnDestroy{
+export class SingleCourseComponent implements OnInit, OnDestroy {
   id_course: string;
   private courseSingleServiceSub: Subscription;
   private courseStudentsServiceSub: Subscription;
   public course: any;
-  public studentsData: any;
+  public studentsData: any = [];
   public dateList: any = ['init'];
   public checkboxForms: any = [];
-  public presence: Presence [] = [];
+  public presence: Presence[] = [];
   public isPresence: boolean = false;
   public isChecked: boolean = true;
   private isClicked: boolean = true;
 
-
-
-
   constructor(
     private route: ActivatedRoute,
-    private courseService:CourseService
-  ) { }
-
+    private courseService: CourseService
+  ) {}
 
   ngOnInit(): void {
-
     this.id_course = this.route.snapshot.paramMap.get('id');
 
-    this.courseStudentsServiceSub = this.courseService
-      .getStudentsData(this.id_course)
-      .subscribe( (data) =>{
-        this.studentsData = data;
-        console.log(data);
-        for (let i=0; i < this.studentsData.dates.length; i++){
-            if (this.studentsData.dates[i].includes(this.getCurrentDate())){
-              this.isChecked = false;
-            }
-        }
-      });
+    this.getStudentsData();
 
     this.courseSingleServiceSub = this.courseService
       .getCoursesById(this.id_course)
       .subscribe((data) => {
         this.course = data;
-        for (let i=0; i<this.course.students.length; i++){
+        for (let i = 0; i < this.course.students.length; i++) {
           this.checkboxForms.push({
-            bool: new FormControl(false)
+            bool: new FormControl(false),
           });
         }
       });
-
-
-
   }
 
-  getCurrentDate(){
+  getStudentsData() {
+    this.courseStudentsServiceSub = this.courseService
+      .getStudentsData(this.id_course)
+      .subscribe((data) => {
+        this.studentsData = data;
+        this.isChecked = true;
+        for (let i = 0; i < this.studentsData.dates.length; i++) {
+          if (this.studentsData.dates[i].includes(this.getCurrentDate())) {
+            this.isChecked = false;
+          }
+        }
+      });
+  }
+
+  getCurrentDate() {
     let today = new Date();
     let dd = String(today.getDate()).padStart(2, '0');
     let mm = String(today.getMonth() + 1).padStart(2, '0');
@@ -109,7 +103,12 @@ export class SingleCourseComponent implements OnInit, OnDestroy{
     this.isPresence = true;
   }
 
-  deleteCol(date: {}) {
-
+  deleteCol(date: String) {
+    this.courseService
+      .deletePresences(this.id_course, date)
+      .subscribe((date) => {
+        console.log(date);
+        this.getStudentsData();
+      });
   }
 }
--- frontend/src/app/services/course.service.ts
+++ frontend/src/app/services/course.service.ts
@@ -1,10 +1,10 @@
 import { CourseRequest } from './../models/couresRequest.model';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { Course } from '../models/course.model';
-import {Presence} from "../models/presence.model";
+import { Presence } from '../models/presence.model';
 
 @Injectable({
   providedIn: 'root',
@@ -18,7 +18,7 @@ export class CourseService {
     return this.http.get(this.API_URL);
   }
 
-  public getCoursesById(id: string): Observable<any>{
+  public getCoursesById(id: string): Observable<any> {
     return this.http.get(this.API_URL + '/' + id);
   }
 
@@ -26,12 +26,23 @@ export class CourseService {
     return this.http.post(this.API_URL, course);
   }
 
-  public addPresence(id: string, presence: Presence[]): Observable<any>{
+  public addPresence(id: string, presence: Presence[]): Observable<any> {
     console.log(presence);
     return this.http.post(this.API_URL + '/' + id + '/presences', presence);
   }
 
-  public getStudentsData(id: string):Observable<any>{
+  public getStudentsData(id: string): Observable<any> {
     return this.http.get(this.API_URL + '/' + id + '/students-data');
+  }
+
+  public deletePresences(id: String, date: String): Observable<any> {
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+      body: date,
+    };
+
+    return this.http.delete(this.API_URL + '/' + id + '/presences', options);
   }
 }
